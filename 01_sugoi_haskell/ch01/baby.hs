0e3f59a43028356257bff66079665bf237e31565
hyz <inzembla@gmail.com>
2019-02-15 22:59:29 +0900

    chapter 1

--- 01_sugoi_haskell/ch01/baby.hs
+++ 01_sugoi_haskell/ch01/baby.hs
@@ -7,3 +7,18 @@ doubleSmallNumber x = if x > 100
                         else x*2
 
 doubleSmallNumber' x = (if x > 100 then x else x*2) + 1
+
+conanO'Brien = "It's a-me, Conan O'Brien!"
+
+lostNumbers = [4,8,15,16,23,42]
+
+addList = [1,2,3,4] ++ [9,10,11,12]
+
+helloWorld = "hello" ++ " " ++ "world"
+
+oneTo20 = [1..20]
+aToz = ['a'..'z']
+kToZ = ['K'..'Z']
+
+func_15_1 = [x*2 | x <- [1..10]]
+func_15_2 = [x*2 | x <- [1..10], x*2 >= 12]
