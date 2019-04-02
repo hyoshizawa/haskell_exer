9180ea64990008ce9cb30419fde3e238a77601e3
hyz <inzembla@gmail.com>
2019-04-02 20:10:40 +0900

    add ch04.hs

---
+++ 01_sugoi_haskell/ch04/ch04.hs
@@ -0,0 +1,20 @@
+-- chapter:4
+maximum' :: (Ord a) => [a] -> a
+maximum' [] = error "maximum of empty list!"
+maximum' [x] = x
+maximum' (x:xs) = max x (maximum' xs)
+
+replicate' :: Int -> a -> [a]
+replicate' n x
+    | n <= 0 = []
+    | otherwise = x : replicate' (n-1) x
+
+take' :: Int -> [a] -> [a]
+take' n _
+    | n <= 0 = []
+take' _ [] = []
+take' n (x:xs) = x : take' (n-1) xs
+
+reverse' :: [a] -> [a]
+reverse' [] = []
+reverse' (x:xs) = reverse' (xs) ++ [x]
